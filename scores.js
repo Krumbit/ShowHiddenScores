06451f69d4214206218d63a7b1f9615db88c5b7d
Krumbit <12988040+Krumbit@users.noreply.github.com>
2024-10-29 16:46:02 -0700

    Initial commit

---
+++ scores.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Hidden Scores
+// @namespace    http://tampermonkey.net/
+// @version      2024-10-29
+// @description  try to take over the world!
+// @author       You
+// @match        https://apclassroom.collegeboard.org/*/assignments?status=completed
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  (function (open) {
+    XMLHttpRequest.prototype.open = function () {
+      this.addEventListener("load", function () {
+        if (this.responseURL.includes("chameleon")) {
+          const assignments = JSON.parse(this.response).assignments;
+          const assignmentsTable = document.getElementsByClassName("student_assignments_table")[0];
+          for (const index in assignments) {
+            const assignment = assignments[index];
+            if (assignment.display_results || assignment.awaiting_scoring) continue;
+
+            const tableRow = assignmentsTable.querySelector(`[aria-label="${assignment.title}"]`);
+            const resultsCell = tableRow.getElementsByClassName("results-cell")[0].firstElementChild.firstElementChild.firstElementChild;
+            const a = document.createElement("a");
+            a.href = `https://apclassroom.collegeboard.org/${getCourseId()}/assessments/results/${assignment.id}`
+            a.innerHTML = `(${assignment.score}/${assignment.max_score})`
+            resultsCell.innerHTML += " "
+            resultsCell.appendChild(a);
+          }
+        }
+      }, false);
+      open.apply(this, arguments);
+    };
+  })(XMLHttpRequest.prototype.open);
+
+})();
+
+function getCourseId() {
+  return document.URL.match(/https:\/\/apclassroom\.collegeboard\.org\/(\d+)\/assignments\?status=completed/)[1];
+}
